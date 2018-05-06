aff698fa54f4adc526194e7b3d1be2b99a7e3ca6
Sandro Rogério <sandrorgguimaraes@gmail.com>
2018-05-05 22:00:14 -0300

    Acrescentado comentários e obtenção do diretório atual.

--- src/utilitarios/path.py
+++ src/utilitarios/path.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
-""" Modulo para gerenciar os caminhos (paths) comuns
+""" Gerencia os caminhos (paths) comuns, com base na localização da aplicação
+    Trabalha com os seguintes 'id':
+        Raiz
+        Export
+        Input
+        Output
+        Config
 """
+
+import os
 
 
 def get_path(id_path):
+    """ Retorna o path referente ao 'id' informado
+    """
     return _Paths_[id_path]
 
 
-# Define Path's / caminhos
+# Define Path's / caminhos a partir da localização atual
 _Paths_ = {}
-_Paths_['Raiz'] = '/home/sandro/Dropbox/python/app_contas/'
+_Paths_['Raiz'] = os.getcwd().replace('/src', '/')
 _Paths_['Export'] = _Paths_['Raiz'] + 'export/'
 _Paths_['Input'] = _Paths_['Raiz'] + 'input/'
 _Paths_['Output'] = _Paths_['Raiz'] + 'output/'
